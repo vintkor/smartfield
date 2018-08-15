7150bc35000905b4148658c637442fdb702dcfcd
Alekxandr <alkv84@yandex.ru>
2018-08-15 12:26:14 +0300

    Добавлен ряд справочников

--- reference_books/admin.py
+++ reference_books/admin.py
@@ -1,3 +1,37 @@
 from django.contrib import admin
+from .models import (
+    Unit,
+    Manufacturer,
+    Agriculture,
+    Seed,
+)
 
-# Register your models here.
+
+@admin.register(Unit)
+class UnitAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Manufacturer)
+class ManufacturerAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Agriculture)
+class AgricultureAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Seed)
+class SeedAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'agriculture',
+        'manufacturer',
+        'seeding_rate',
+        'seeding_rate_unit',
+    )
+    list_filter = (
+        'agriculture',
+        'manufacturer',
+    )
--- reference_books/models.py
+++ reference_books/models.py
@@ -1,3 +1,70 @@
 from django.db import models
+from django.utils.translation import ugettext as _
 
-# Create your models here.
+
+class Unit(models.Model):
+    """
+    Единицы измерения
+    """
+    short_name = models.CharField(max_length=30, verbose_name=_('Краткое название'))
+    long_name = models.CharField(max_length=20, verbose_name=_('Полное название'))
+
+    class Meta:
+        verbose_name = _('Единица измерения')
+        verbose_name_plural = _('Единицы измерения')
+
+    def __str__(self):
+        return self.short_name
+
+
+class Manufacturer(models.Model):
+    """
+    Произвадители
+    """
+    title = models.CharField(max_length=250, verbose_name=_('Название'))
+    address = models.CharField(max_length=300, verbose_name=_('Адрес'), blank=True, null=True)
+    phone = models.PositiveIntegerField(blank=True, null=True)
+    desc = models.TextField(blank=True, null=True, verbose_name=_('Описание'))
+    website = models.URLField(blank=True, null=True, verbose_name=_('URL'))
+
+    class Meta:
+        verbose_name = _('Произвадитель')
+        verbose_name_plural = _('Произвадители')
+
+    def __str__(self):
+        return self.title
+
+
+class Agriculture(models.Model):
+    """
+    Что выращиваем? Агрокультура - кукуруза, буряк, пшеница
+    """
+    title = models.CharField(max_length=250, verbose_name=_('Название'))
+    photo = models.ImageField(upload_to='reference_books', blank=True, null=True)
+    desc = models.TextField(blank=True, null=True, verbose_name=_('Описание'))
+
+    class Meta:
+        verbose_name = _('Агрокультура')
+        verbose_name_plural = _('Агрокультуры')
+
+    def __str__(self):
+        return self.title
+
+
+class Seed(models.Model):
+    """
+    Семена
+    """
+    agriculture = models.ForeignKey(Agriculture, on_delete=models.CASCADE, verbose_name=_('Агрокультура'))
+    title = models.CharField(max_length=250, verbose_name=_('Название'))
+    desc = models.TextField(blank=True, null=True, verbose_name=_('Описание'))
+    manufacturer = models.ForeignKey(Manufacturer, on_delete=models.CASCADE, verbose_name=_('Произвадитель'))
+    seeding_rate = models.DecimalField(verbose_name=_('Норма высева'), decimal_places=3, max_digits=15)
+    seeding_rate_unit = models.ForeignKey(Unit, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = _('Семена')
+        verbose_name_plural = _('Семена')
+
+    def __str__(self):
+        return self.title
